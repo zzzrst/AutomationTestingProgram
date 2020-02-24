ae220caf90d2fcb366827d7e7eebaa72eb0d8041
chronoreaper <chris.dd.29@gmail.com>
2020-02-24 16:28:21 -0500

    Added logger and info objects

---
+++ SeleniumProgram/SeleniumProgram/InformationObject.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="InformationObject.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace AutomationTestingProgram
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// An information class that contains information needed by other objects/methods.
+    /// </summary>
+    public static class InformationObject
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether to respect the repeat for value.
+        /// </summary>
+        public static bool RespectRepeatFor { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to run AODA.
+        /// </summary>
+        public static bool RespectRunAODAFlag { get; set; }
+
+        /// <summary>
+        /// Gets or sets the location to save the log.
+        /// </summary>
+        public static string LogSaveFileLocation { get; set; }
+
+        /// <summary>
+        /// Gets or sets the location to save the CSV file.
+        /// </summary>
+        public static CSVLogger CSVLogger { get; set; }
+
+        /// <summary>
+        /// Gets or sets the object to get the test set data from.
+        /// </summary>
+        public static ITestSetData TestSetData { get; set; }
+
+        /// <summary>
+        /// Gets or sets the object to get the test case data from.
+        /// </summary>
+        public static ITestCaseData TestCaseData { get; set; }
+
+        /// <summary>
+        /// Gets or sets the object to get the test step data from.
+        /// </summary>
+        public static ITestStepData TestStepData { get; set; }
+
+        /// <summary>
+        /// Gets or sets the testing driver to run the testing program on.
+        /// </summary>
+        public static ITestingDriver TestingDriver { get; set; }
+    }
+}
---
+++ SeleniumProgram/SeleniumProgram/Logger.cs
@@ -0,0 +1,95 @@
+﻿// <copyright file="Logger.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace AutomationTestingProgram
+{
+    using log4net;
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Xml;
+
+    /// <summary>
+    /// Logger class to be used. \n
+    /// Reference 1: https://stackify.com/log4net-guide-dotnet-logging/ \n
+    /// Reference 2: https://www.dotnetdepth.in/2018/06/how-to-use-log4net-in-net-core-20.html.
+    /// </summary>
+    public static class Logger
+    {
+        private static readonly string LOGCONFIGFILE = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\log4net.config";
+
+        private static readonly ILog Log = GetLogger(typeof(Logger));
+
+        /// <summary>
+        /// Allows the user to log a debug message.
+        /// </summary>
+        /// <param name="message">The debug message to be logged.</param>
+        public static void Debug(object message)
+        {
+            Log.Debug(message);
+        }
+
+        /// <summary>
+        /// Allows the user to log an error message.
+        /// </summary>
+        /// <param name="message">The error message to be logged.</param>
+        public static void Error(object message)
+        {
+            Log.Error(message);
+        }
+
+        /// <summary>
+        /// Allows the user to log a fatal message.
+        /// </summary>
+        /// <param name="message">The fatal message to be logged.</param>
+        public static void Fatal(object message)
+        {
+            Log.Fatal(message);
+        }
+
+        /// <summary>
+        /// Allows the user to log an info message.
+        /// </summary>
+        /// <param name="message">The info message to be logged.</param>
+        public static void Info(object message)
+        {
+            Log.Info(message);
+        }
+
+        /// <summary>
+        /// Allows the user to log a warning message.
+        /// </summary>
+        /// <param name="message">The warning message to be logged.</param>
+        public static void Warn(object message)
+        {
+            Log.Warn(message);
+        }
+
+        /// <summary>
+        /// Returns the type for the logger.
+        /// </summary>
+        /// <param name="type">Class this logger is under.</param>
+        /// <returns>ILog instance to interact with.</returns>
+        private static ILog GetLogger(Type type)
+        {
+            ILog logger = LogManager.GetLogger(type);
+            SetLog4NetConfiguration();
+            return logger;
+        }
+
+        /// <summary>
+        /// Sets up the Log4Net Configuration.
+        /// </summary>
+        private static void SetLog4NetConfiguration()
+        {
+            XmlDocument log4netConfig = new XmlDocument();
+            log4netConfig.Load(File.OpenRead(LOGCONFIGFILE));
+
+            var repo = LogManager.CreateRepository(
+                Assembly.GetEntryAssembly(), typeof(log4net.Repository.Hierarchy.Hierarchy));
+
+            log4net.Config.XmlConfigurator.Configure(repo, log4netConfig["log4net"]);
+        }
+    }
+}
