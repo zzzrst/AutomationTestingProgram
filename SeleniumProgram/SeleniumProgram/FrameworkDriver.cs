8f44ade8d5f94c6a9393d068aaa43bffc9d72aa1
chronoreaper <chris.dd.29@gmail.com>
2020-02-24 13:34:07 -0500

    Initial set up

---
+++ SeleniumProgram/SeleniumProgram/FrameworkDriver.cs
@@ -0,0 +1,15 @@
+﻿namespace SeleniumProgram
+{
+    using System;
+
+    /// <summary>
+    /// Main program.
+    /// </summary>
+    internal class FrameworkDriver
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
