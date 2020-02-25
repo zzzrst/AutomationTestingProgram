a9b39357d6239053c24bb9338eb4db8d36c7665d
chronoreaper <chris.dd.29@gmail.com>
2020-02-25 13:45:46 -0500

    Create TextDriver.cs

---
+++ SeleniumProgram/SeleniumProgram/TestingData/DataDrivers/TextDriver.cs
@@ -0,0 +1,44 @@
+﻿namespace AutomationTestingProgram.TestingData.DataDrivers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using AutomationTestSetFramework;
+
+    /// <summary>
+    /// A singelton of a textDriver.
+    /// </summary>
+    public class TextDriver : ITestCaseData, ITestSetData, ITestStepData
+    {
+        static TextDriver()
+        {
+        }
+
+        private TextDriver()
+        {
+        }
+
+        /// <summary>
+        /// Gets the driver for the text driver.
+        /// </summary>
+        public static TextDriver Driver { get; } = new TextDriver();
+
+        /// <inheritdoc/>
+        public Dictionary<string, string> GetArguments()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public ITestCase GetNextTestCase()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public ITestStep GetNextTestStep()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
