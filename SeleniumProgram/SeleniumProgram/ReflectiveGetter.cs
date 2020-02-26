118e5d292bd60303741cc491be17323e3c11fec9
chronoreaper <chris.dd.29@gmail.com>
2020-02-26 09:28:34 -0500

    Moved Reflective Getter

--- SeleniumProgram/SeleniumProgram/ReflectiveGetter.cs
+++ SeleniumProgram/SeleniumProgram/ReflectiveGetter.cs
@@ -2,7 +2,7 @@
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
-namespace AutomationTestingProgram.AutomationFramework
+namespace AutomationTestingProgram
 {
     using System;
     using System.Collections.Generic;
