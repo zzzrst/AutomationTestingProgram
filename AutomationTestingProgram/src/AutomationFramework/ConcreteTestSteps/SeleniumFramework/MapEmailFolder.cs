cff66885aea820af63a4912cb7497abacee8980a
chronoreaper <chris.dd.29@gmail.com>
2020-05-05 15:13:03 -0400

    Refactoring code

--- AutomationTestingProgram/src/AutomationFramework/ConcreteTestSteps/SeleniumFramework/MapEmailFolder.cs
+++ AutomationTestingProgram/src/AutomationFramework/ConcreteTestSteps/SeleniumFramework/MapEmailFolder.cs
@@ -19,13 +19,9 @@ namespace AutomationTestingProgram.AutomationFramework
         /// <inheritdoc/>
         public override void Execute()
         {
-            string emailFolderLocation = string.Empty;
-            string username = string.Empty;
-            string password = string.Empty;
-
-            emailFolderLocation = ((DatabaseStepData)TestStepData).GetEnvironmentEmailNotificationFolder(GetEnvironmentVariable(EnvVar.Environment));
-            username = ((DatabaseStepData)TestStepData).GetGlobalVariableValue("WINDOW ACCOUNT USERNAME");
-            password = ((DatabaseStepData)TestStepData).GetGlobalVariableValue("WINDOW ACCOUNT PASSWORD");
+            string emailFolderLocation = ((DatabaseStepData)TestStepData).GetEnvironmentEmailNotificationFolder(GetEnvironmentVariable(EnvVar.Environment));
+            string username = ((DatabaseStepData)TestStepData).GetGlobalVariableValue("WINDOW ACCOUNT USERNAME");
+            string password = ((DatabaseStepData)TestStepData).GetGlobalVariableValue("WINDOW ACCOUNT PASSWORD");
 
             Process p = new Process();
             ProcessStartInfo startInfo = new ProcessStartInfo
