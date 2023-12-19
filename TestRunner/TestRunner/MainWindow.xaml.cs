265e6efbc059f3a3493c5bbbd6eed674fdba05f2
victor-zheng-codes <victork.zheng@mail.utoronto.ca>
2023-12-19 00:52:53 -0500

    configured more of the components with right naming

--- TestRunner/TestRunner/MainWindow.xaml.cs
+++ TestRunner/TestRunner/MainWindow.xaml.cs
@@ -23,20 +23,40 @@ namespace TestRunner
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            Console.WriteLine("ButtonClicked");
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void Environment_TextBoxChanged(object sender, TextChangedEventArgs e)
         {
-
+            Console.WriteLine("Textbox Text changed");
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            Console.WriteLine("Combobox selection changed");
         }
 
         private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            Console.WriteLine("Textbox text changed 1");
+        }
+
+        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
+        {
+            Console.WriteLine("textbox text changed 2");
+        }
+
+        private void FileNameTextbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void PlanNameValues_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ProjectNameValues_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
