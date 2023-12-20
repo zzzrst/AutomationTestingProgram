e52bd7e070e11fd8cc36faff99af491215311886
victor-zheng-codes <victork.zheng@mail.utoronto.ca>
2023-12-20 18:16:13 -0500

    About to wait an extra X seconds printed out for robustness of program

--- AutomationTestingProgram/src/AutomationFramework/ConcreteTestSteps/SeleniumFramework/ActionObject/ClickObject/ClickButton.cs
+++ AutomationTestingProgram/src/AutomationFramework/ConcreteTestSteps/SeleniumFramework/ActionObject/ClickObject/ClickButton.cs
@@ -2,6 +2,12 @@
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
+//using Microsoft.Extensions.Configuration;
+using System.Threading;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+
 namespace AutomationTestingProgram.AutomationFramework
 {
     /// <summary>
@@ -11,5 +17,19 @@ namespace AutomationTestingProgram.AutomationFramework
     {
         /// <inheritdoc/>
         public override string Name { get; set; } = "Click Button";
+
+        /// <inheritdoc/>
+        protected override string HTMLWhiteListTag { get; set; } = "WebButton_HTMLTags";
+
+        /// <inheritdoc/>
+        public override void Execute()
+        {
+            base.Execute();
+
+            // sleep an additional 3.5 seconds after clicking
+            int sleepAmt = int.Parse(ConfigurationManager.AppSettings["CLICK_BUTTON_WAIT_TIME"]);
+            Logger.Info($"About to sleep an additional {sleepAmt} seconds");
+            Thread.Sleep(sleepAmt);
+        }
     }
 }
