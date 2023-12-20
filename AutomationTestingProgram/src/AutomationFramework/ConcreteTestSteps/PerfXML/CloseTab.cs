4ee8720c5ed37489f07bc34f6d1ed86a6c72a5fa
victor-zheng-codes <victork.zheng@mail.utoronto.ca>
2023-12-20 18:14:25 -0500

    made differenciation between close tab and close browser

--- AutomationTestingProgram/src/AutomationFramework/ConcreteTestSteps/PerfXML/CloseTab.cs
+++ AutomationTestingProgram/src/AutomationFramework/ConcreteTestSteps/PerfXML/CloseTab.cs
@@ -12,15 +12,35 @@ namespace AutomationTestingProgram.AutomationFramework
     public class CloseTab : TestStep
     {
         /// <inheritdoc/>
-        public override string Name { get; set; } = "CloseTab";
+        public override string Name { get; set; } = "Close Tab";
 
         /// <inheritdoc/>
         public override void Execute()
         {
             base.Execute();
-            int tabIndex = Convert.ToInt32(this.Arguments["value"]);
-            InformationObject.TestAutomationDriver.SwitchToTab(tabIndex);
-            InformationObject.TestAutomationDriver.CloseBrowser();
+            string stringVal = this.Arguments["value"];
+
+            // if stringVal is null, then log as error
+            if (stringVal == string.Empty)
+            {
+                Logger.Error("Not a valid use of Close Tab, it is string.Empty");
+                this.TestStepStatus.RunSuccessful = false;
+                return;
+            }
+
+            int tabIndex = Convert.ToInt32(stringVal);
+
+            try
+            {
+                // here we indicate the index of the tab that we are wanting to close
+                InformationObject.TestAutomationDriver.SwitchToTab(tabIndex);
+                InformationObject.TestAutomationDriver.CloseBrowser(false);
+            }
+            catch (Exception ex)
+            {
+                this.TestStepStatus.RunSuccessful = false;
+                Logger.Error(ex);
+            }
         }
     }
 }
