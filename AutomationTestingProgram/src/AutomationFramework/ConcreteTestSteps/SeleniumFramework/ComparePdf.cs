f8f881df64ae3a562354258c2005c09680973e40
victor-zheng-codes <victork.zheng@mail.utoronto.ca>
2023-12-20 18:53:21 -0500

    modfied header

--- AutomationTestingProgram/src/AutomationFramework/ConcreteTestSteps/SeleniumFramework/ComparePdf.cs
+++ AutomationTestingProgram/src/AutomationFramework/ConcreteTestSteps/SeleniumFramework/ComparePdf.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="ComparePdf.cs" company="PlaceholderCompany">
+﻿// <copyright file="ComparePDF.cs" company="PlaceholderCompany">
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
