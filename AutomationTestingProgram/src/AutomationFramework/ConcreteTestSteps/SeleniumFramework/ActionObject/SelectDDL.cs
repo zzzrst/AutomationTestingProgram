b38dd37ff8bd73ef95bbb4b863eb14494ddfc974
victor-zheng-codes <victork.zheng@mail.utoronto.ca>
2023-12-20 18:39:35 -0500

    Select DDL improvements

--- AutomationTestingProgram/src/AutomationFramework/ConcreteTestSteps/SeleniumFramework/ActionObject/SelectDDL.cs
+++ AutomationTestingProgram/src/AutomationFramework/ConcreteTestSteps/SeleniumFramework/ActionObject/SelectDDL.cs
@@ -2,24 +2,42 @@
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
+using System;
+using System.Threading;
+
 namespace AutomationTestingProgram.AutomationFramework
 {
     /// <summary>
     /// This class executes the action of selecting a value from the specified dropdownlist.
     /// </summary>
-    public class SelectDDL : TestStep
+    /// I think this was configured incorrectly, changed from Test Step to ActionObject
+    // public class SelectDDL : ActionObject
+    public class SelectDDL : ActionObject
     {
-        /// <inheritdoc/>
+        /// <inheritdoc/>e
         public override string Name { get; set; } = "Select DDL";
+
+        /// <inheritdoc/>
+        protected override string HTMLWhiteListTag { get; set; } = "WebList_HTMLTag";
 
         /// <inheritdoc/>
         public override void Execute()
         {
             base.Execute();
-            string xPath = this.Arguments["object"];
+
+            // this is possibly not an xpath and rather an html id
+            // string xPath = this.Arguments["object"];
             string selection = this.Arguments["value"];
-            InformationObject.TestAutomationDriver.SelectValueInElement(xPath, selection);
+
+            Console.WriteLine("xpath value: " + this.XPath);
+            Console.WriteLine("selection: " + selection);
+
+            // InformationObject.TestAutomationDriver.SelectValueInElement(xPath, selection);
+            InformationObject.TestAutomationDriver.SelectValueInElement(this.XPath, selection);
             InformationObject.TestAutomationDriver.WaitForLoadingSpinner();
+
+            this.TestStepStatus.Actual += " successfully clicked " + selection + " using xpath" + this.XPath;
+            this.TestStepStatus.RunSuccessful = true;
         }
     }
 }
