1fd8563e99c3ff7e3ecf0c907560f9a853fb5991
victor-zheng-codes <victork.zheng@mail.utoronto.ca>
2023-12-20 18:37:38 -0500

    Populate text box is now an Action

--- AutomationTestingProgram/src/AutomationFramework/ConcreteTestSteps/SeleniumFramework/ActionObject/PopulateTextBox.cs
+++ AutomationTestingProgram/src/AutomationFramework/ConcreteTestSteps/SeleniumFramework/ActionObject/PopulateTextBox.cs
@@ -4,10 +4,13 @@
 
 namespace AutomationTestingProgram.AutomationFramework
 {
+    using System;
+    using System.Collections.Generic;
+
     /// <summary>
     /// This test step is the same as testStepXml.
     /// </summary>
-    public class PopulateTextBox : TestStep
+    public class PopulateTextBox : ActionObject
     {
         /// <inheritdoc/>
         public override string Name { get; set; } = "PopulateTextBox";
@@ -16,11 +19,25 @@ namespace AutomationTestingProgram.AutomationFramework
         public override void Execute()
         {
             base.Execute();
-            string xPath = this.Arguments["object"];
+
             string text = this.Arguments["value"];
-            Logger.Warn("This test step only does xpaths for now.");
-            InformationObject.TestAutomationDriver.PopulateElement(xPath, text);
-            InformationObject.TestAutomationDriver.WaitForLoadingSpinner();
+            Logger.Warn("This test step only does xpaths and html ids for now.");
+
+            try
+            {
+                InformationObject.TestAutomationDriver.PopulateElement(this.XPath, text);
+                InformationObject.TestAutomationDriver.WaitForLoadingSpinner();
+                this.TestStepStatus.RunSuccessful = true; // rest of try clause is skipped if it fails
+                this.TestStepStatus.Actual = "Successfully populated text box"; // clear to default
+            }
+            catch (Exception ex)
+            {
+                Logger.Info("Trying to Populate Text Box");
+                this.ShouldExecuteVariable = true;
+                this.TestStepStatus.RunSuccessful = false;
+                this.TestStepStatus.Actual = "Failure in Populating TextBox using xpath: " + this.XPath;
+                this.HandleException(ex);
+            }
         }
     }
 }
