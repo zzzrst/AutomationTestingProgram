4001aecb32309ea1b7dd490b7a3dc0842d20d760
victor-zheng-codes <victork.zheng@mail.utoronto.ca>
2023-12-20 18:28:16 -0500

    verify link comments

--- AutomationTestingProgram/src/AutomationFramework/ConcreteTestSteps/SeleniumFramework/ActionObject/VerifyObject/VerifyObjectAvailability/VerifyLinkAvailability.cs
+++ AutomationTestingProgram/src/AutomationFramework/ConcreteTestSteps/SeleniumFramework/ActionObject/VerifyObject/VerifyObjectAvailability/VerifyLinkAvailability.cs
@@ -5,7 +5,12 @@
 namespace AutomationTestingProgram.AutomationFramework
 {
     /// <summary>
-    /// This test step to verify the availability of an image.
+    /// This test step to verify the availability of a link.
+    /// Note that verifying a link requries a FLAG. 
+    /// If the FLAG is EXIST, this means that the text is visible.
+    /// If the FLAG is DOES NOT EXIST, this means that the text is not visible.
+    /// If the FLAG is ENABLED, this means that the text is CLICKABLE and VISIBLE
+    /// If the FLAG is DISABLED, this means that the text is NOT CLICKABLE or NOT VISIBLE.
     /// </summary>
     public class VerifyLinkAvailability : VerifyObjectAvailability
     {
