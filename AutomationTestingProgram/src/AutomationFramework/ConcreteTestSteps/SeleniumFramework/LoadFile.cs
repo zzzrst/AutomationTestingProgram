5660d96534b0483ed21f003128589e1273c13c60
victor-zheng-codes <victork.zheng@mail.utoronto.ca>
2023-12-20 18:55:32 -0500

    load file logic

--- AutomationTestingProgram/src/AutomationFramework/ConcreteTestSteps/SeleniumFramework/LoadFile.cs
+++ AutomationTestingProgram/src/AutomationFramework/ConcreteTestSteps/SeleniumFramework/LoadFile.cs
@@ -1,11 +1,15 @@
 ﻿// <copyright file="LoadFile.cs" company="PlaceholderCompany">
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
-/*
-using System;
 
 namespace AutomationTestingProgram.AutomationFramework
 {
+    using System;
+    using System.Linq;
+    using AutomationTestinProgram.Helper;
+    using AutomationTestSetFramework;
+    using OpenQA.Selenium;
+
     /// <summary>
     /// This test step is the same as testStepXml.
     /// </summary>
@@ -18,62 +22,33 @@ namespace AutomationTestingProgram.AutomationFramework
         public override void Execute()
         {
             base.Execute();
-            bool passed = false;
-            string obj = this.Arguments["object"];
-            string value = this.Arguments["value"];
-            string property = this.TestObject.Attribute;
-            string message = $"Web File with attribute '{property}' and value of '{obj}' ";
-            Test_Object objFileUpload = new Test_Object("webFile", property, obj);
-            objFileUpload.AddAttribute("tag", "input");
+            string locator;
+            string filePath = this.Arguments["value"];
+            string property = this.Arguments["comment"];
 
-            lock (AutoItDriver.Obj)
+            // add implementation to read html ids. See Load File examples
+            if (property == "html id" || property == string.Empty)
             {
-                WebElement webFileUpload = new WebElement(this.BrowserDriver.GetBrowser(), objFileUpload, this.Timeout);
-
-                // check if web file upload is found
-                if (!webFileUpload.IsFound)
-                {
-                    message += "could not be found.";
-                }
-                else
-                {
-                    // attempt to upload to web file
-                    try
-                    {
-                        while (AutoItDriver.DialogBoxExist(this.Alm.AlmBrowser))
-                        {
-                        }
-
-                        if (passed = webFileUpload.Click(opensModal: true))
-                        {
-                            string uploadDialogName = AutoItDriver.UploadDialogBoxTitle(this.Alm.AlmBrowser);
-                            if (passed = AutoItDriver.PopulateWinDialogBox(this.Alm.AlmBrowser, uploadDialogName, this.Timeout, value))
-                            {
-                                message += $"successfully chose file '{value}'.";
-                            }
-                            else
-                            {
-                                message += $"could not choose file '{value}'.";
-                            }
-                        }
-                        else
-                        {
-                            message += $"could not select input to choose file '{value}'.";
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        message += $"did not choose file '{value}', but found something wrong: '{ex.Message}'.";
-                    }
-                }
-
-                // create and return test status
-                this.TestStatus = new Test_Status()
-                {
-                    Message = message,
-                    Pass = passed,
-                };
+                // Ideally we get: //*[@id='Name']
+                locator = $"//*[@id='{this.Arguments["object"]}']";
+                Logger.Info("xpath from HTML ID: " + locator);
             }
+            else
+            {
+                // original
+                Logger.Warn("Likely error in implementation of Load File");
+                locator = this.Arguments["object"];
+            }
+
+            filePath = FilePathResolver.Resolve(filePath); // resolve the file path in case it's as K folder
+
+            Logger.Info("Attempting to upload file to: " + filePath + " with button:  " + locator);
+
+            if (InformationObject.TestAutomationDriver.VerifyAttribute("type", "file", locator))
+            {
+                ((TestingDriver.SeleniumDriver)InformationObject.TestAutomationDriver).GetWebElement(locator).SendKeys(filePath);
+                this.TestStepStatus.Actual = "Successfully inputted the value into Load File.";
+            }
+        }
     }
 }
-*/
