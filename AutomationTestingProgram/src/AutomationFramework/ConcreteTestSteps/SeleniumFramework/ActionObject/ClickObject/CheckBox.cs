df72f5ccc189e8353dbbb59cb1723e07e853a3e2
victor-zheng-codes <victork.zheng@mail.utoronto.ca>
2023-12-20 18:15:24 -0500

    add whitelist to Check Box

--- AutomationTestingProgram/src/AutomationFramework/ConcreteTestSteps/SeleniumFramework/ActionObject/ClickObject/CheckBox.cs
+++ AutomationTestingProgram/src/AutomationFramework/ConcreteTestSteps/SeleniumFramework/ActionObject/ClickObject/CheckBox.cs
@@ -13,5 +13,8 @@ namespace AutomationTestingProgram.AutomationFramework
     {
         /// <inheritdoc/>
         public override string Name { get; set; } = "Check Box";
+
+        /// <inheritdoc/>
+        protected override string HTMLWhiteListTag { get; set; } = "WebCheckBox_HTMLTags";
     }
 }
