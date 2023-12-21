e3be707fd4bbc4da6ee5692c21ef06c7c208af75
victor-zheng-codes <victork.zheng@mail.utoronto.ca>
2023-12-20 19:02:46 -0500

    Configure exception handling better

---
+++ AutomationTestingProgram/src/AutomationFramework/Loggers_and_Reporters/HTMLReporter/Exceptions/CannotPostTestRun.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="CannotPostTestRun.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace AutomationTestingProgram
+{
+    using System;
+
+    /// <summary>
+    /// Defines the <see cref="CannotPostTestRun" />.
+    /// </summary>
+    public class CannotPostTestRun : Exception
+    {
+        /// <summary>
+        /// Defines the ErrorMsg.
+        /// </summary>
+        public const string ErrorMsg = "ALM could not post test run: ";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CannotPostTestRun"/> class.
+        /// </summary>
+        public CannotPostTestRun()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CannotPostTestRun"/> class.
+        /// </summary>
+        /// <param name="message">The message<see cref="string"/>.</param>
+        public CannotPostTestRun(string message)
+            : base(message)
+        {
+        }
+    }
+}
