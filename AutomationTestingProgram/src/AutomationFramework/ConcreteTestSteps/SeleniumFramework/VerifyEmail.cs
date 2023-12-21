f3edd0fe5f0c5cf838f396c4a1d30fa078b4ca39
victor-zheng-codes <victork.zheng@mail.utoronto.ca>
2023-12-20 19:00:34 -0500

    Verify email and ensure that the file path is correct before beginning

--- AutomationTestingProgram/src/AutomationFramework/ConcreteTestSteps/SeleniumFramework/VerifyEmail.cs
+++ AutomationTestingProgram/src/AutomationFramework/ConcreteTestSteps/SeleniumFramework/VerifyEmail.cs
@@ -4,6 +4,7 @@
 
 namespace AutomationTestingProgram.AutomationFramework
 {
+    using AutomationTestinProgram.Helper;
     using System;
     using System.Diagnostics;
     using System.IO;
@@ -21,24 +22,54 @@ namespace AutomationTestingProgram.AutomationFramework
         public override void Execute()
         {
             base.Execute();
-            string emailPath = Path.Combine(this.Arguments["comments"], "Emails");
-            string emailShortLIst = Path.Combine(this.Arguments["comments"], "EmailList.txt");
 
-            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            string resultFilePath = desktop + "\\compareEmailResult.txt";
+            // we have to use the argument comment, not comments
+            string emailPath = Path.Combine(this.Arguments["comment"], "Emails");
+            string emailShortLIst = Path.Combine(this.Arguments["comment"], "EmailList.txt");
+
+            string logFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Log\\";
+
+            // we should resolve the filepath first
+            string expectedFilePath = this.Arguments["value"];
+
+            expectedFilePath = FilePathResolver.Resolve(expectedFilePath);
+
+            if (expectedFilePath.Contains("K:\\"))
+            {
+                string tempFileLoc = logFolder + "temp_email_compare.eml";
+
+                File.Copy(expectedFilePath, tempFileLoc, true);
+                expectedFilePath = tempFileLoc;
+
+                Logger.Info("Successfully copied to new file on local disk: " + expectedFilePath);
+            }
+
+            Logger.Info("Comparing email from: " + expectedFilePath + " to file located at " + emailPath);
+
+            string resultFilePath = logFolder + "\\compareEmailResult.txt";
+
+            Logger.Info("CSV logger location: " + InformationObject.CSVLogger);
+            Logger.Info("CSV logger location: " + InformationObject.LogSaveFileLocation);
+            Logger.Info("Result file path is: " + resultFilePath);
 
             string executingPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             string powershellFile = Path.Combine(executingPath, "scripts", "compareEmail.ps1");
 
             string command = $"powershell -Executionpolicy Bypass \"&'{powershellFile}' " +
                              $"-emailPath '{emailPath}' " +
-                             $"-ExpectedPath '{this.Arguments["value"]}' " +
+                             $"-ExpectedPath '{expectedFilePath}' " +
                              $"-emailShortList '{emailShortLIst}' " +
-                             $"-patternToRemove '{this.Arguments["object"]}' " +
                              $"-resultFilePath '{resultFilePath}' \"";
 
             this.TestStepStatus.RunSuccessful = this.RunProcess(command) == 0 && !File.Exists(resultFilePath);
             this.TestStepStatus.Actual = this.TestStepStatus.RunSuccessful ? "Email Matched" : "Email did not match. Please check desktop for results.";
+
+            // attach email attachment log if it fails
+            if(File.Exists(resultFilePath))
+            {
+                InformationObject.TestSetData.AddAttachment(resultFilePath);
+            }
+
             Logger.Info(this.TestStepStatus.Actual);
         }
 
