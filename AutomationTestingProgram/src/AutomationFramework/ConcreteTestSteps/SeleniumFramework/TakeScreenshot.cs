670a9a7999966877a4f03834f1bec2e515969e37
victor-zheng-codes <victork.zheng@mail.utoronto.ca>
2023-12-20 19:00:08 -0500

    Take screenshot (new entire screenshot)

---
+++ AutomationTestingProgram/src/AutomationFramework/ConcreteTestSteps/SeleniumFramework/TakeScreenshot.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="TakeScreenshot.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace AutomationTestingProgram.AutomationFramework
+{
+    using System.IO;
+    using System.Reflection;
+    using System;
+
+    /// <summary>
+    /// This test step to compare pdfs.
+    /// </summary>
+    public class TakeScreenshot : TestStep
+    {
+        /// <inheritdoc/>
+        public override string Name { get; set; } = "TakeScreenshot";
+
+        /// <inheritdoc/>
+        public override void Execute()
+        {
+            base.Execute();
+
+            string fileName = $"{DateTime.Now:yyyy_MM_dd-hh_mm_ss_tt}.png";
+
+            string path_ex = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            InformationObject.TestAutomationDriver.TakeEntireScreenshot($"{path_ex}\\Log\\NEW_{fileName}", false);
+
+            InformationObject.Reporter.AddTestStepScreenshot("Screenshot.png", $"{path_ex}\\Log\\NEW_{fileName}", "screenshot after taking screenshot");
+        }
+    }
+}
